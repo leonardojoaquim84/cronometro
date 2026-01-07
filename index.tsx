38837f99286d19e544c5bfaa1294487c88b98c94
Leonardo <leonardojoaquim84@gmail.com>
2026-01-07 01:20:34 -0300

    update reack para visualizar pagina html

--- index.tsx
+++ index.tsx
@@ -1,17 +1,16 @@
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
 
-if (!container) {
-  console.error("Failed to find the root element");
-} else {
+if (container) {
   const root = createRoot(container);
   root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   );
+} else {
+  console.error("Não foi possível encontrar o elemento #root no DOM.");
 }
